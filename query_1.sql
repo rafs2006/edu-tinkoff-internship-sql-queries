2cc205e1934d843e9bbc312951790746af46236b
rafs2006 <rafs2006@yandex.ru>
2021-10-23 23:09:07 +0800

    one more time

--- query_1.sql
+++
@@ -1,4 +0,0 @@
-SELECT name, flight_dt FROM flights
-JOIN pilots ON flights.second_pilot_id = pilots.pilot_id
-WHERE (flight_dt BETWEEN '2021-08-01' AND '2021-08-31') AND (destination = 'SVO')
-ORDER BY flight_dt;
